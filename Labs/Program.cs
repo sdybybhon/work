2ee2095b80b369a7faec508ec0dd02b383488ba8
sdybybhon <144716291+sdybybhon@users.noreply.github.com>
2023-12-25 21:35:22 +0500

    Update Program.cs
    FixExplanations

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -14,31 +14,27 @@ namespace lab4
     public class Program
     {
         public static void Main()
-        //Здесь я запрашиваю у пользователя математическое выражение, после вызываю методы для преобразования выражения и вывожу ответ. 
         {
             Console.WriteLine("Введите математическое выражение:");
-            string expression = Console.ReadLine(); //Считывая выражение
+            string expression = Console.ReadLine(); 
 
-            List<object> tokens = GetTokens(expression); //Работаю с методами 
-            List<object> rpnTokens = ConvertToRPN(tokens); //Работаю с методами 
-            double result = EvaluateRPN(rpnTokens); //Работаю с методами 
+            List<object> tokens = GetTokens(expression); 
+            List<object> rpnTokens = ConvertToRPN(tokens);  
+            double result = EvaluateRPN(rpnTokens); 
 
-            Console.WriteLine("Результат: " + result); //Выводим результат)
+            Console.WriteLine("Результат: " + result); 
         }
 
         public static List<object> GetTokens(string expression)
-        //Этот метод принимает выражение пользователя и преобразует его с список токенов. 
-        //Далее метод пробегается по всему выражению и каждый его символ закидывает(число или оператор) закидывает в список токенов.
+        //Пробегаюсь по всему выражению и каждый его символ закидывает(число или оператор) закидывает в список токенов.
         //Если символ это цифра, то он попадает в токены. 
         //Если символ это оператор, то текущий токен добавляет в список токенов, а после оператор добавляется в список токенов.
-        //По итогу, если ластовый токен является числом, он также добавляется в список токенов.
-        //Дальше смотрим метод ConvertToRPN.
-        //PS: Если символ является `(` или `)`, он добавляется в список токенов. 
+        //Если символ является `(` или `)`, он добавляется в список токенов. 
         {
-            List<object> tokens = new List<object>(); //создаем список, в который будем добавлять токены
+            List<object> tokens = new List<object>(); 
             string currentToken = "";
 
-            foreach (char c in expression) //проходимся по каждому символу в выражении и смотреть на объяснение метода выше
+            foreach (char c in expression) 
             {
                 if (Char.IsDigit(c))
                 {
@@ -55,12 +51,12 @@ namespace lab4
                 }
                 else if (c == '(' || c == ')')
                 {
-                    if (!String.IsNullOrEmpty(currentToken)) //проверяем, не является ли текущий токен пустым.
+                    if (!String.IsNullOrEmpty(currentToken)) 
                     {
                         tokens.Add(double.Parse(currentToken));
                         currentToken = "";
                     }
-                    tokens.Add(c); //  возвращаем список токенов.
+                    tokens.Add(c);
                 }
             }
 
@@ -73,17 +69,13 @@ namespace lab4
         }
 
         public static List<object> ConvertToRPN(List<object> tokens)
-        //Принимает список токенов и работаем с ОПЗ.                        Это себе: (ЕСЛИ ЗАБЫЛ, ТО СМОТРЕТЬ http://decoding.dax.ru/practic/polishrecord/polishrecord.html)
-        //Юзаем стек, чтобы раскидать всё по приоритетам.                   Смотреть метод GetPrecedence
-        //Если попадается оператор, то метод проверяет приоритет оператора. 
-        //Если оператор на вершине стека имеет больший или равный приоритет, то закидывает операторы из стека в конечный список, пока это вообще возмножно.
-        //Затем текущий оператор добавляется в стек
-        //Ну и в конце концов, все оставшиеся операторы из стека добавляются в конечный список.
+        //Принимает список токенов и работаем с ОПЗ.                       
+        //Юзаем стек, чтобы раскидать всё по приоритетам
         {
-            List<object> output = new List<object>(); //создаем список, в который будем добавлять токены в ОПЗ
-            Stack<object> operatorStack = new Stack<object>(); //наш стек
+            List<object> output = new List<object>(); 
+            Stack<object> operatorStack = new Stack<object>(); 
 
-            foreach (object token in tokens) //пробегаемся по всем токенм
+            foreach (object token in tokens) 
             {
                 if (token is double)
                 {
@@ -121,24 +113,23 @@ namespace lab4
                 }
             }
 
-            while (operatorStack.Count > 0) //проверяем, пока в стеке операторов есть элементы
+            while (operatorStack.Count > 0) 
             {
                 output.Add(operatorStack.Pop());
             }
 
-            return output; //возвращаем список RPN токенов.
+            return output; 
         }
         public static double EvaluateRPN(List<object> rpnTokens)
-        //Здесь мы уже получили список токенов в ОПЗ.                        Это себе: (ЕСЛИ ЗАБЫЛ, ТО СМОТРЕТЬ http://decoding.dax.ru/practic/polishrecord/polishrecord.html)
+        //Здесь мы уже получили список токенов в ОПЗ.                       
         //Здесь юзаю стек операндов для хранения чисел. 
         //Если токен в моём списке это число то оно добавляется в стек операндов.
         //Если токен в моём списке это оператор,то  метод извлекает два операнда из стека.
         //Затем выполяет математическую операцию и добавляет результат обратно в стек операндов.
-        //Ну и в конце у нас результат вычисления выражения. Оно находится на вершине стека операндов и возвращается.
         {
-            Stack<double> operandStack = new Stack<double>(); //создаем стек, в который будем добавлять операнды
+            Stack<double> operandStack = new Stack<double>(); 
 
-            foreach (object token in rpnTokens) //проходимся по каждому токену в списке RPN токенов
+            foreach (object token in rpnTokens) 
             {
                 if (token is double)
                 {
@@ -177,10 +168,8 @@ namespace lab4
         }
 
         public static int GetPrecedence(char op)
-        //Это используется в методе ConvertToRPN
+        //Для ConvertToRPN
         //Здесь принимаем оператор и возвращаем его приоритет.
-        //У операторов "+" и "-" приоритет 1. 
-        //У операторов "*" и "/" приоритет 2. 
         //Если оператор не является ни одним из указанных, возвращается 0.
         {
             if (op == '+' || op == '-')
